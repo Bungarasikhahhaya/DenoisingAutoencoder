e4470e928e32a99141a677886cacaac0d67b669d
nisakhrn <nkhairun044@gmail.com>
2025-12-17 13:45:46 +0700

    update landing

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-# Force commit test
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from tensorflow.keras.models import load_model
@@ -13,19 +12,17 @@ app = Flask(__name__)
 CORS(app)
 
 # ----- KONFIGURASI MODEL -----
-MODEL_PATH = os.path.join(
-    "model",
-    "DenoisingAutoencoder-20251202T172437Z-1-001",
-    "DenoisingAutoencoder",
-    "saved_models",
-    "dataweights.106-0.00.keras",
-)
+MODEL_PATH = r"D:\Anis\sem 5\prak ai\project uas\face_clean\DenoisingAutoencoder\backend\model\DenoisingAutoencoder-20251202T172816Z-1-001\DenoisingAutoencoder\saved_models\dataweights.115-0.00.keras"
+
+print("MODEL_PATH =", MODEL_PATH)
+print("EXISTS? ", os.path.isfile(MODEL_PATH))
 
 IMG_SIZE = (256, 256)  # ganti sesuai input model
-IS_GRAY = False       # True kalau model 1-channel (grayscale)
+IS_GRAY = False        # True kalau model 1-channel (grayscale)
 
 # Load model sekali di awal
 model = load_model(MODEL_PATH)
+
 
 def preprocess_image(file_bytes: bytes) -> np.ndarray:
     # Baca gambar dari bytes
@@ -47,6 +44,7 @@ def preprocess_image(file_bytes: bytes) -> np.ndarray:
     array = np.expand_dims(array, axis=0)       # (1,h,w,c)
     return array
 
+
 def array_to_base64(img_array: np.ndarray) -> str:
     # img_array: (h,w,c) atau (h,w)
     img_array = np.clip(img_array * 255.0, 0, 255).astype("uint8")
@@ -59,10 +57,12 @@ def array_to_base64(img_array: np.ndarray) -> str:
     byte_im = buf.getvalue()
     return base64.b64encode(byte_im).decode("utf-8")
 
+
 # Endpoint test untuk cek CORS
 @app.route("/api/ping", methods=["GET"])
 def ping():
     return jsonify({"message": "pong"})
+
 
 @app.route("/api/denoise", methods=["POST"])
 def denoise():
@@ -87,6 +87,7 @@ def denoise():
     b64 = array_to_base64(denoised)
 
     return jsonify({"image_base64": b64})
+
 
 if __name__ == "__main__":
     print("Starting Flask...")
